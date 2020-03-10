5d2c7db60cb7c54ea082e4eb96ed91c0402bcd51
Kellen Presta <kpresta04@gmail.com>
2020-03-10 13:57:54 -0500

    103

--- src/components/ExpenseDashboardPage.js
+++ src/components/ExpenseDashboardPage.js
@@ -1,9 +1,11 @@
 import React from "react";
 import ExpenseList from "./ExpenseList";
 import { connect } from "react-redux";
+import ExpenseListFilters from "./ExpenseListFilters";
 
 const ExpenseDashboardPage = () => (
   <div>
+    <ExpenseListFilters />
     <ExpenseList />
   </div>
 );
